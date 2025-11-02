db281602e21ab32f6f5cc7b966f9d8126a1478e1
Caier <kosmoziemia@gmail.com>
2025-11-02 01:38:52 +0100

    shortcut script

---
+++ scripts/short.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+# a group of alias commands for faster interfacing with docker
+# `source short.sh`
+
+_assign_short_names__() {
+    local SCRIPT_DIR=$(dirname $(realpath $1))
+
+    alias dcc="docker compose -f \"$SCRIPT_DIR/../core.yaml\""
+    alias dcr="docker compose -f \"$SCRIPT_DIR/../ran.yaml\""
+}
+
+_assign_short_names__ $0
+unset -f _assign_short_names__
